9f122efe6344a5f0df0cce309a381d7aae9b9721
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-11-22 20:27:09 +0100

    Remove unnecessary braces.

--- Item07_Distinguish_between()_and_{}_when_creating_objects/initializer_lists1.cpp
+++ Item07_Distinguish_between()_and_{}_when_creating_objects/initializer_lists1.cpp
@@ -9,19 +9,16 @@
 
 class Widget {
 public:
-  Widget(int i, bool b) { std::cout << "Widget(int, bool)" << std::endl; };     // ctors not declaring
-  Widget(int i, double d) { std::cout << "Widget(int double)" << std::endl; };  // std::initializer_list params
+  Widget(int i, bool b) { std::cout << "Widget(int, bool)" << std::endl; };      // ctors not declaring
+  Widget(int i, double d) { std::cout << "Widget(int, double)" << std::endl; };  // std::initializer_list params
 };
 
 int main()
 {
   // In constructor calls, parentheses and braces have the same meaning as
   // long as std::initializer_list parameters are not involved:
-  {
-    Widget w1(10, true);  // calls first ctor
-    Widget w2{10, true};  // also calls first ctor
-    Widget w3(10, 5.0);   // calls second ctor
-    Widget w4{10, 5.0};   // also calls second ctor
-  }
-
+  Widget w1(10, true);  // calls first ctor
+  Widget w2{10, true};  // also calls first ctor
+  Widget w3(10, 5.0);   // calls second ctor
+  Widget w4{10, 5.0};   // also calls second ctor
 }
