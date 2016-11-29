011cbfcc4b4860b152160823a81491f1be60e968
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-11-29 21:58:48 +0100

    Add references for explicitly typed initializer idiom.

--- Item06_Use_the_explicitly_typed_initializer_idiom_when_auto_deduces_undesired_types/explicitly_typed_initializer_idiom.cpp
+++ Item06_Use_the_explicitly_typed_initializer_idiom_when_auto_deduces_undesired_types/explicitly_typed_initializer_idiom.cpp
@@ -3,6 +3,14 @@
  *
  *   The typed initializer idiom forces auto to deduce the type you want it to
  *   have.
+ *
+ * See also comments and discussions in [1-3].
+ *
+ * References:
+ *
+ *   [1] http://www.aristeia.com/BookErrata/emc++-errata.html
+ *   [2] http://stackoverflow.com/questions/25607216/why-should-i-prefer-the-explicitly-typed-initializer-idiom-over-explicitly-giv
+ *   [3] https://herbsutter.com/2013/08/12/gotw-94-solution-aaa-style-almost-always-auto/
  */
 
 #include <vector>
