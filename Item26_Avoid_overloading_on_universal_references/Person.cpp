1cdbca5da468855092dddb9e57aae2932c55eb08
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-03-05 23:07:32 +0100

    Minor cleanup of key idea for Item 26.

--- Item26_Avoid_overloading_on_universal_references/Person.cpp
+++ Item26_Avoid_overloading_on_universal_references/Person.cpp
@@ -1,11 +1,10 @@
 /*
  * Key idea:
  *
- *   Perfect-forwarding constructor will generally match better than the copy
- *   constructor, Which may produce compilation errors if the passed parameter
- *   is not of the same type as the member variable initialized.To force on
- *   using the copy constructor you might pass a const lvalue.
- *
+ *   The perfect-forwarding constructor will generally match better than the
+ *   copy constructor, which may produce compilation errors if the passed
+ *   parameter is not of the same type as the member variable initialized.  To
+ *   force on using the copy constructor, you might pass a const lvalue.
  */
 
 #include <iostream>
