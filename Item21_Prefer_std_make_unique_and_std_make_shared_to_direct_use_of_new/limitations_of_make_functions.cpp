fd25ef6916e774a176a9ad8782742750c71dbd24
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-03-06 00:04:44 +0100

    Avoid redefinition of pBigObj.

--- Item21_Prefer_std_make_unique_and_std_make_shared_to_direct_use_of_new/limitations_of_make_functions.cpp
+++ Item21_Prefer_std_make_unique_and_std_make_shared_to_direct_use_of_new/limitations_of_make_functions.cpp
@@ -66,39 +66,41 @@ int main()
 
 
   // Limitation 3 (only for make_shared)
-  auto pBigObj =                         // create very large
-    std::make_shared<ReallyBigType>();   // object via
-                                         // std::make_shared
-  
-               // create std::shared_ptrs and std::weak_ptrs to
-               // large object, use them to work with it
-  
-               // final std::shared_ptr to object destroyed here,
-               // but std::weak_ptrs to it remain
-  
-               // during this period, memory formerly occupied
-               // by large object remains allocated
-  
-               // final std::weak_ptr to object destroyed here;
-               // memory for control block and object is released
+  {
+      auto pBigObj =                         // create very large
+        std::make_shared<ReallyBigType>();   // object via
+                                            // std::make_shared
+      
+                  // create std::shared_ptrs and std::weak_ptrs to
+                  // large object, use them to work with it
+      
+                  // final std::shared_ptr to object destroyed here,
+                  // but std::weak_ptrs to it remain
+      
+                  // during this period, memory formerly occupied
+                  // by large object remains allocated
+      
+                  // final std::weak_ptr to object destroyed here;
+                  // memory for control block and object is released
+  }
+  {
+      std::shared_ptr<ReallyBigType> pBigObj(new ReallyBigType);
+                                          // create very large
+                                          // object via new
 
-  std::shared_ptr<ReallyBigType> pBigObj(new ReallyBigType);
-                                       // create very large
-                                       // object via new
+                  // as before, create std::shared_ptrs and
+                  // std::weak_ptrs to object, use them with it
 
-               // as before, create std::shared_ptrs and
-               // std::weak_ptrs to object, use them with it
+                  // final std::shared_ptr to object destroyed here,
+                  // but std::weak_ptrs to it remain
+                  // memory for object is deallocated
 
-               // final std::shared_ptr to object destroyed here,
-               // but std::weak_ptrs to it remain
-               // memory for object is deallocated
+                  // during this period, only memory for
+                  // the control block remains allocated
 
-               // during this period, only memory for
-               // the control block remains allocated
-
-               // final std::weak_ptr to object destroyed here;
-               // memory for control block and object is released
-
+                  // final std::weak_ptr to object destroyed here;
+                  // memory for control block and object is released
+  }
 
   // Exception-unsafe call
 
